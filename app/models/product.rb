a70ab0ee24c5e06495ba45ad15ffa1ec06de6009
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-26 16:57:24 -0500

    add is_discounted?, tax, and total methods to the product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    price < 10
+  end
+
+  def tax
+    tax = price * 0.09
+    return tax
+  end
+
+  def total
+    total = tax + price
+    return total
+
+  end
+
 end
