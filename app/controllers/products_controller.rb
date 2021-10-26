e20bce802b93d8de340b0a63757bbcec3ea43bd1
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-26 15:52:14 -0500

    hardcode create functionality in controller, working.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,15 @@
 class ProductsController < ApplicationController
   def create
-    render json: {message:"hello"}
+    new_product = Product.new(
+      
+      name: "Chewing Gum", 
+      price: 1, 
+      image_url: "https://www.eatthis.com/wp-content/uploads/sites/4/2021/02/chewing-gum.jpg", 
+      description: "chewable breath refresher."
+    
+    )
+    new_product.save
+    render json: new_product.as_json
+    
   end
 end
