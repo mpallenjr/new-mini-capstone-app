3c19acdc12dad10855316da3c9430c3fcd26d3ba
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-28 19:38:43 -0500

    realize dont need migration, link products and categories to CategoryProducts table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
+  has_many :category_products
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_234601) do
+ActiveRecord::Schema.define(version: 2021_10_29_000547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_10_28_234601) do
     t.integer "quantity"
     t.integer "supplier_id"
     t.integer "image_id"
+    t.integer "category_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
